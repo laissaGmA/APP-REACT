9b554533de14f526edd991c05b53884a6b737185
laissaGmA <110406866+laissaGmA@users.noreply.github.com>
2022-10-17 19:36:52 -0300

    Add files via upload

---
+++ src/components/Resume/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ResumeItem from "../ResumeItem";
+import * as C from "./styles";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
+
+const Resume = ({ income, expense, total }) => {
+  return (
+    <C.Container>
+      <ResumeItem
+        title="Depósitos"
+        Icon={FaRegArrowAltCircleUp}
+        value={income}
+      />
+      <ResumeItem
+        title="Saques"
+        Icon={FaRegArrowAltCircleDown}
+        value={expense}
+      />
+      <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+    </C.Container>
+  );
+};
+
+export default Resume;
